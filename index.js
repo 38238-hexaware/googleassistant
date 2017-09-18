1721ce109c1799f65e617a4bb76f1c9b7b69e82d
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-18 17:20:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -98,36 +98,36 @@ function callApi(req, res) {
 var myKeys = Object.keys(flightstatus) 
 newflightss=flightstatus[myKeys.indexOf(flightstatuses)!==-1?flightstatuses:""];
                                 //app1.ask(`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd} , Arrival ${estimateda}. Schedule Departure ${scheduledd} , Arrival ${scheduleda}. Do u want to know anything else?`);
-    app1.ask(app1.buildRichResponse()
-    // Create a basic card and add it to the rich response
+//     app1.ask(app1.buildRichResponse()
+//     // Create a basic card and add it to the rich response
 
-    .addSimpleResponse('Do you need anything else?')
-    .addBasicCard(app1.buildBasicCard(`**Departure:** \n\n${datedep}\n\n${scheduledd.substring(0, scheduledd.length-4)}\n *Est:* ${estimatedd.substring(0, estimatedd.length-4)} \n\n ${airports} \n\n**Arrival:** \n\n${datearr}\n\n${scheduleda.substring(0, scheduleda.length-4)}\n *Est:* ${estimateda.substring(0, estimateda.length-4)}\n\n${airportd}`)
-      .setTitle(`Flight Status ${sourcecarrier} => ${destcarrier}`)
-      .setImage(`https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=10&size=200x200&maptype=roadmap&markers=anchor:topleft%7Cicon:https://www.dropbox.com/s/coom3cu3rdijeby/rsz_1rsz_airplane-512.png?raw=1%7C40.714728,-73.998672&key=AIzaSyCsB3zqvC5gR7LrAG7_kyiBkk17dQiFUes', 'Image alternate text`)
-    )
-  );              
-//===========================================                                
-//   app1.askWithList(app1.buildRichResponse()
-//     .addSimpleResponse({displayText:'Do you want to know anything else?',speech:`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd.substring(0, estimatedd.length-4)} , Arrival ${estimateda.substring(0, estimateda.length-4)}. Schedule Departure ${scheduledd.substring(0, scheduledd.length-4)} , Arrival ${scheduleda.substring(0, scheduleda.length-4)}. Do u want to know anything else?`})
-                   
-//     .addSuggestions(['Yes', 'No']),
-//     app1.buildList('Flight Status')
-//     // Add the first item to the list
-//     .addItems(app1.buildOptionItem('option2')
-//       .setTitle(`D ${datedep}`)
-//       .setDescription(`${scheduledd.substring(0, scheduledd.length-4)}\n\nEst:${estimatedd.substring(0, estimatedd.length-4)}`)
-//        .setImage('https://www.dropbox.com/s/l1h4x5r6ox5f60q/rsz_departure.png?raw=1', 'Departure')
-//       )
-//     // Add the second item to the list
-//     .addItems(app1.buildOptionItem('option1')
-//       .setTitle(`A ${datearr}`)
-//       .setDescription(`${scheduleda.substring(0, scheduleda.length-4)}\n\nEst:${estimateda.substring(0, estimateda.length-4)}`)
-//       .setImage('https://www.dropbox.com/s/mcw8pgxq5akdn0v/rsz_arrival.png?raw=1', 'Arrival')
-//     ).addItems(app1.buildOptionItem('option3').setTitle(`${sourcecarrier} => ${destcarrier}`)
-//       .setDescription(`${airports} - ${airportd}`)
+//     .addSimpleResponse('Do you need anything else?')
+//     .addBasicCard(app1.buildBasicCard(`**Departure:** \n\n${datedep}\n\n${scheduledd.substring(0, scheduledd.length-4)}\n *Est:* ${estimatedd.substring(0, estimatedd.length-4)} \n\n ${airports} \n\n**Arrival:** \n\n${datearr}\n\n${scheduleda.substring(0, scheduleda.length-4)}\n *Est:* ${estimateda.substring(0, estimateda.length-4)}\n\n${airportd}`)
+//       .setTitle(`Flight Status ${sourcecarrier} => ${destcarrier}`)
+//       .setImage(`https://www.dropbox.com/s/g7avz78npsqcm2n/flight647x404_101116021054.jpg?raw=1', 'Flight Status`)
 //     )
-//   );
+//   );              
+//===========================================                                
+  app1.askWithList(app1.buildRichResponse()
+    .addSimpleResponse({displayText:'Do you want to know anything else?',speech:`Flight is ${newflightss} from ${airports} ${countrys} to ${airportd} ${countryd}. Estimated Departure ${estimatedd.substring(0, estimatedd.length-4)} , Arrival ${estimateda.substring(0, estimateda.length-4)}. Schedule Departure ${scheduledd.substring(0, scheduledd.length-4)} , Arrival ${scheduleda.substring(0, scheduleda.length-4)}. Do u want to know anything else?`})
+                   
+    .addSuggestions(['Yes', 'No']),
+    app1.buildList('Flight Status')
+    // Add the first item to the list
+    .addItems(app1.buildOptionItem('option2')
+      .setTitle(`D ${datedep}`)
+      .setDescription(`${scheduledd.substring(0, scheduledd.length-4)}\n\nEst:${estimatedd.substring(0, estimatedd.length-4)}`)
+       .setImage('https://www.dropbox.com/s/l1h4x5r6ox5f60q/rsz_departure.png?raw=1', 'Departure')
+      )
+    // Add the second item to the list
+    .addItems(app1.buildOptionItem('option1')
+      .setTitle(`A ${datearr}`)
+      .setDescription(`${scheduleda.substring(0, scheduleda.length-4)}\n\nEst:${estimateda.substring(0, estimateda.length-4)}`)
+      .setImage('https://www.dropbox.com/s/mcw8pgxq5akdn0v/rsz_arrival.png?raw=1', 'Arrival')
+    ).addItems(app1.buildOptionItem('option3').setTitle(`${sourcecarrier} => ${destcarrier}`)
+      .setDescription(`${airports} - ${airportd}`)
+    )
+  );
  //=====================================================
 
 //  app1.ask(app1.buildRichResponse()
